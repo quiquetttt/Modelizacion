eb604dcdc89f3fbbd5810cda8d736b950c1371f3
angelaloon <97341516+angelaloon@users.noreply.github.com>
2024-05-08 12:51:46 +0200

    Algoritmo final 3

--- algoritmo.py
+++ algoritmo.py
@@ -2,19 +2,34 @@ import networkx as nx
 import matplotlib.pyplot as plt
 def main():
     G = construccionTriangulo(4)
+    etiquetarGrafo(G)
+    for nodo in G.nodes:
+        print(nodo)
+        print(G.nodes[nodo])
+
+def algoritmo():
+    G = construccionTriangulo(4)
     precioGrafo(G)
+    etiquetarGrafo(G)
+    centro = sacarCentro(G)
+    for nodo in centro:
+        tomarDecision(G,nodo)
     
 
-def numeroVertices(x):
-    """Declaro el total con los 3 vertices iniciales y los 3*x vertices laterales"""
-    total = 3 + x*3
-    internos = 0
-    """Calculo el numero de vertices internos que habra"""
-    for k in range (x):
-        internos += k
-    return total+internos
+def tomarDecision():
+    pass
+
     
+
+
+
+
+
+"""Metodos para construir el triangulo"""
 """x = numero de vertices laterales (sin contar las dos esquinas)"""
+
+
+
 def construccionTriangulo(x):
     n = numeroVertices(x)
     G = nx.Graph()
@@ -46,16 +61,100 @@ def construccionTriangulo(x):
     G.add_edges_from(aristas)
     return G
 
+def numeroVertices(x):
+    """Declaro el total con los 3 vertices iniciales y los 3*x vertices laterales"""
+    total = 3 + x*3
+    internos = 0
+    """Calculo el numero de vertices internos que habra"""
+    for k in range (x):
+        internos += k
+    return total+internos
 
 
-    
+"""Metodos para diseccionar el triangulo"""
 
+def sacarLaterales(G):
+    laterales = [0]
+    G.nodes[0]["decision"] = 3
+    laterales.extend(lateralIzq(G))
+    laterales.extend(lateralAbj(G))
+    laterales.extend(lateralDer(G))
+    return laterales
+
+def sacarCentro(G):
+    medio = G.nodes
+    laterales = sacarLaterales(G)
+    medio = [elemento for elemento in medio if elemento not in laterales]
+    return medio
+
+
+
+def lateralIzq(G):
+    res = []
+    altura = G.graph["altura"]
+    nodo = 0 
+    for i in range(1,altura):
+        nodo = nodo+i
+        G.nodes[nodo]["decision"] = 1
+        res.append(nodo)    
+    return res
+
+def lateralDer(G):
+    res = []
+    altura = G.graph["altura"]
+    nodo = 0 
+    for i in range(1,altura):
+        nodo = nodo +i +1
+        G.nodes[nodo]["decision"] = 3
+        res.append(nodo)
+    G.nodes[res[-1]]["decision"] =2
+    res = res[::-1]
+    return res
+
+def lateralAbj(G):
+    res = []
+    resIzq = lateralIzq(G)
+    resDer = lateralDer(G)
+    primerElem = resIzq[-1]
+    UltimoElem = resDer[0]
+    for i in range(primerElem+1, UltimoElem):
+        G.nodes[i]["decision"] = 2
+        res.append(i)
+    return res
+
+"""Metodos para etiquetar los nodos"""
+
+def etiquetarGrafo(G):
+    etiquetar_lat(G)
+    etiquetar_centro(G)
+
+def etiquetar_lat(G):
+    lat = sacarLaterales(G)
+    pintura =0
+    for elemento in lat:
+        pintura = (pintura%3)+1
+        G.nodes[elemento]["pintura"] = pintura
+
+def etiquetar_centro(G):
+    medio = sacarCentro(G)
+    latIzq = lateralIzq(G)
+    del latIzq[0]
+    latIzq.pop(-1)
+    i = 1
+    for elem in latIzq:
+        pintura = G.nodes[elem]["pintura"]
+        for j in range(0, i):
+            pintura = (pintura % 3) + 1
+            G.nodes[medio[0]]["pintura"] = pintura
+            del medio[0]
+        i += 1
+
+
+"""Metodos para poner precio a cada nodo"""
 
 def precioGrafo(G):
     for nodo in G.nodes:
         G.nodes[nodo]["precio"] = precioNodo(nodo,G)
-
-
 
 
 def precioNodo(nodo,G):
@@ -68,8 +167,8 @@ def precioNodo(nodo,G):
         precio = [1,0,4]
     else:
         precio.append(precioIzquierda(G,nodo))
-        precio.append(precioDerecha(G,nodo))
         precio.append(precioAbajo(G,nodo))
+        precio.append(precioDerecha(G,nodo))
     return precio
 
 def precioIzquierda(G,nodo):
@@ -98,18 +197,6 @@ def precioDerecha(G,nodo):
 
 def precioAbajo(G,nodo):
     return  G.graph["altura"] - G.nodes[nodo]["capa"]
-
-        
-
-def precio(G,nodo):
-    pass
-
-
-def pruebas():
-    for k in range(1,6):
-        print(k)
-
-
 
 
 if __name__ == "__main__":
