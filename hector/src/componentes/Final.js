8e9cc21a4dde72523e531cda5455d0ed1e97ec21
danid <danidelalama@gmail.com>
2024-05-16 01:23:55 +0200

    Final.js

--- hector/src/componentes/Final.js
+++ hector/src/componentes/Final.js
@@ -10,10 +10,16 @@ export function Final() {
   if (!occupantNames || !final) return null;
   return (
     <div>
-      <p>Dan Puterisimo</p>
-      <p>Resultado Final: {final}</p>
+      <div className="title-container">
+        <h1>Enhorabuena, aqui tienes la distribución justa de la renta</h1>
+      </div>
+      <div class="vertical-bar left-bar"></div>
+      <div class="vertical-bar right-bar"></div>
+      <div className="final-message-box">
+        <h3>Resultado Final: {final}</h3>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Final;
